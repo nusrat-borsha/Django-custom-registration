684eac72d805698117ef31f244c69238bdefeb4d
nusrat-borsha <nuameeen@gmail.com>
2020-05-13 16:42:37 +0600

    "added models for accounts app"

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    is_customer = models.BooleanField(default=False)
+    is_employee = models.BooleanField(default=False)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE, primary_key = True)
+    phone_number = models.CharField(max_length=20)
+    location = models.CharField(max_length=20)
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE, primary_key = True)
+    phone_number = models.CharField(max_length=20)
+    designation = models.CharField(max_length=20)
+
+
+
