4050e30769655fab27a7851c8343d3c402f3addb
nusrat-borsha <nuameeen@gmail.com>
2020-05-13 17:19:49 +0600

    "added models to admin site"

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, Customer, Employee
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Customer)
+admin.site.register(Employee)
