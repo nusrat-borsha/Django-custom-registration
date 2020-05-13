c4dd506ba8dea8862720378a49d871534a1de63b
nusrat-borsha <nuameeen@gmail.com>
2020-05-13 17:11:56 +0600

    "added views for registration"

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout
+from django.shortcuts import redirect, render
+from django.views.generic import CreateView
+from .form import CustomerSignUpForm, EmployeeSignUpForm
+from .models import User
 
-# Create your views here.
+def register(request):
+    return render(request, '../templates/register.html')
+
+class customer_register(CreateView):
+    model = User
+    form_class = CustomerSignUpForm
+    template_name = '../templates/customer_register.html'
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('/')
+
+class employee_register(CreateView):
+    model = User
+    form_class = EmployeeSignUpForm
+    template_name = '../templates/employee_register.html'
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('/')
