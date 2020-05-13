aa5c96e82e00e1bea45f9d4626296b731b3a8217
nusrat-borsha <nuameeen@gmail.com>
2020-05-13 17:15:18 +0600

    "added paths for accounts app"

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .import  views
+
+urlpatterns=[
+     path('register/',views.register, name='register'),
+     path('customer_register/',views.customer_register.as_view(), name='customer_register'),
+     path('employee_register/',views.employee_register.as_view(), name='employee_register'),
+]
