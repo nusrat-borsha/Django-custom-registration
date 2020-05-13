5daee12dc3217ce2218c7088625f54ecaf92d3da
nusrat-borsha <nuameeen@gmail.com>
2020-05-13 18:08:21 +0600

    "created index page"

--- demo_register/urls.py
+++ demo_register/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('accounts/', include('accounts.urls')),
+    path('', include('pages.urls')),
     path('admin/', admin.site.urls),
 ]
