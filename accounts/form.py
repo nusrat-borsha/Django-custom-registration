258ce6ddab59d888af6d5fa0b2572cd6466eb98e
nusrat-borsha <nuameeen@gmail.com>
2020-05-13 16:45:18 +0600

    "created forms for registration"

---
+++ accounts/form.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.db import transaction
+from .models import User,Customer,Employee
+
+class CustomerSignUpForm(UserCreationForm):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+    phone_number = forms.CharField(required=True)
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+    
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.is_customer = True
+        user.first_name = self.cleaned_data.get('first_name')
+        user.last_name = self.cleaned_data.get('last_name')
+        user.save()
+        customer = Customer.objects.create(user=user)
+        customer.phone_number=self.cleaned_data.get('phone_number')
+        customer.save()
+        return user
+
+class EmployeeSignUpForm(UserCreationForm):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+    designation = forms.CharField(required=True)
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.is_employee = True
+        user.first_name = self.cleaned_data.get('first_name')
+        user.last_name = self.cleaned_data.get('last_name')
+        user.save()
+        employee = Employee.objects.create(user=user)
+        employee.designation=self.cleaned_data.get('designation')
+        employee.save()
+        return user
--- demo_register/settings.py
+++ demo_register/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+AUTH_USER_MODEL = 'accounts.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
