48a252545291c729073257340656629fc737939b
nusrat-borsha <nuameeen@gmail.com>
2020-05-15 03:24:23 +0600

    added login and logout to authenticate user

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,6 @@ urlpatterns=[
      path('register/',views.register, name='register'),
      path('customer_register/',views.customer_register.as_view(), name='customer_register'),
      path('employee_register/',views.employee_register.as_view(), name='employee_register'),
+     path('login/',views.login_request, name='login'),
+     path('logout/',views.logout_view, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout,authenticate
 from django.shortcuts import redirect, render
+from django.contrib import messages
 from django.views.generic import CreateView
 from .form import CustomerSignUpForm, EmployeeSignUpForm
+from django.contrib.auth.forms import AuthenticationForm
 from .models import User
 
 def register(request):
@@ -26,3 +28,25 @@ class employee_register(CreateView):
         user = form.save()
         login(self.request, user)
         return redirect('/')
+
+
+def login_request(request):
+    if request.method=='POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None :
+                login(request,user)
+                return redirect('/')
+            else:
+                messages.error(request,"Invalid username or password")
+        else:
+                messages.error(request,"Invalid username or password")
+    return render(request, '../templates/login.html',
+    context={'form':AuthenticationForm()})
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
